1e9eb4bc8166200981a5cde996bdd53cac7900a7
EchoDSun <echodsun@163.com>
2019-06-18 23:59:59 +0800

    主要是图片上传功能

---
+++ src/main/java/com/jack/enterprise/BookKeepingServerApplication.java
@@ -0,0 +1,13 @@
+package com.jack.enterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookKeepingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookKeepingServerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jack/enterprise/controllers/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+*
+* @author sunjian
+*
+* @creation 2019年6月1日 下午9:29:11
+*/
+package com.jack.enterprise.controllers;
